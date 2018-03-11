886600b443a64f374af2bc604cbb1f05ec50fff6
Jimmy Moon <ragingwind@gmail.com>
2018-03-11 21:49:38 +0900

    fix missing option

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const NextWorkboxWebpackPlugin = require('next-workbox-webpack-plugin')
 
 module.exports = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
-    webpack(config, { isServer, dev, buildId, config: {distDir} }) {
-      if (!options.defaultLoaders) {
+    webpack(config, { isServer, dev, buildId, defaultLoaders, config: {distDir} }) {
+      if (!defaultLoaders) {
         throw new Error(
           'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
         )
