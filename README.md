f9732cb70107288c4a89119389f71fb2ee231338
Jimmy Moon <ragingwind@gmail.com>
2018-03-11 21:17:22 +0900

    add webpack pluginint

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 > Next.js plugins for workbox and PWA
 
+# Installation
+
+```sh
+npm install --save next-workbox
+```
+or
+```
+yarn add next-workbox
+```
+
+# Usage
+
+```js
+// next.config.js
+const withWorkbox = require('next-workbox')
+module.exports = withWorkbox()
+```
+
 ## License
 
 MIT © [Jimmy Moon](https://ragingwind.me)
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+const NextWorkboxWebpackPlugin = require('next-workbox-webpack-plugin')
 
-	opts = opts || {};
+module.exports = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, { isServer, dev, buildId, config: {distDir} }) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+        )
+      }
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
-};
+      if (!isServer && !dev) {
+        config.plugins.push(new NextWorkboxWebpackPlugin({
+          distDir,
+          buildId,
+          ...nextConfig
+        }))
+      }
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    }
+  })
+}
